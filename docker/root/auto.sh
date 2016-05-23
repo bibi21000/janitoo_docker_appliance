45e4549755cdb8dac0e7121619c9b8a307d2529f
bibi21000 <bibi21000@gmail.com>
2016-05-23 23:28:25 +0200

    Auto-commit

--- docker/root/auto.sh
+++ docker/root/auto.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
-cd /root/glances
-#git pull origin develop
-#You must loop here
-#~ nice -n 19 python -m glances
 nice -n 19 top
 /root/shutdown.sh
