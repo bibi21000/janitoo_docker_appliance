31b57f0c470fc4a2c6b798a0447dded7a8f05165
bibi21000 <bibi21000@gmail.com>
2016-06-18 23:21:32 +0200

    Auto-commit

--- docker/root/auto.sh
+++ docker/root/auto.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm /opt/janitoo/pid/*
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 nice -n 19 top -b
 /root/shutdown.sh
--- docker/root/shell.sh
+++ docker/root/shell.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm /opt/janitoo/pid/*
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 #You must loop here
 /bin/bash
